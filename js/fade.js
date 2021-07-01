4d0c612bc4b7ca6c81ebb1631fd167eb05c2b6f1
Tony Tran <32388674+ttran2@users.noreply.github.com>
2021-07-01 10:05:55 +0200

    Implement fade-in and fade-out animation

---
+++ js/fade.js
@@ -0,0 +1,24 @@
+const observerOptions = {
+  root: null,
+  rootMargin: "0px",
+  threshold: 0.7
+};
+
+function observerCallback(entries, observer) {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      // fade in observed elements that are in view
+      console.log(entry);
+      entry.target.classList.replace('fade-out', 'fade-in');
+      console.log(entry);
+    } else {
+      // fade out observed elements that are not in view
+      entry.target.classList.replace('fade-in', 'fade-out');
+    }
+  });
+}
+
+const observer = new IntersectionObserver(observerCallback, observerOptions);
+
+const fadeElms = document.querySelectorAll('.fade');
+fadeElms.forEach(el => observer.observe(el));
